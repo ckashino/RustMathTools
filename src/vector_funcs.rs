b1e38a77320e68ff6acf1b2e2908fa43da2a8eb3
ckashino <cjkashino@gmail.com>
2021-09-28 13:16:54 -0700

    implemented add and subtract for vec3

--- src/vector_funcs.rs
+++ src/vector_funcs.rs
@@ -12,14 +12,27 @@ pub struct Vec3 {
 
 impl Display for Vec3{
     fn fmt (&self, f: &mut Formatter) -> fmt::Result{
-
-
-        write!(f, "{}, {}, {}", self.x, self.y, self.z)    
-
-    }
-    
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)    
+    }    
 }
+impl std::ops::Add for Vec3 {
+    type  Output = Vec3;
 
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {x: self. x + other.x, y: self.y + other.y, z: self.z + other.z}
+    }
+
+
+}
+impl std::ops::Sub for Vec3 {
+    type  Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {x: (self.x) - (other.x), y: (self.y) - (other.y), z: (self.z) - (other.z)}
+    }
+
+
+}
 pub fn new_vec(x: i32, y:i32, z:i32) -> Vec3{
     Vec3{
         x: x,
@@ -35,8 +48,6 @@ pub fn cross_prod(v1: Vec3, v2: Vec3) -> Vec3{
 }
 
 pub fn dot_prod(v1: Vec3, v2: Vec3) -> i32{
-
     v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
-
-
 }
+
